af91f73b1ff34ff0b7530316aed503eb6da3d61a
jerobernardi <jerobernardi@gmail.com>
2021-03-29 00:01:51 -0300

    Implementado destruccion falta testear, copiarGrafo sin terminar

--- veinteveinte.h
+++ veinteveinte.h
@@ -18,8 +18,19 @@ typedef GrafoSt *Grafo;
 
 Grafo ConstruccionDelGrafo(void);
 
+/**
+ * Esta funcion libera la memoria alocada para el grafo G incluyendo la memoria
+ * alocada para los vertices de G.
+ * @param Grafo G
+ */
 void DestruccionDelGrafo(Grafo G);
 
+/**
+ * Esta funcion crea una nueva instancia de un grafo G y copia todos sus 
+ * atributos
+ * @param Grafo G
+ * @return Grafo Copy
+ */
 Grafo CopiarGrafo(Grafo G);
 
 //funciones para extraer datos del grafo. u32 esta definida en el .h de arriba
