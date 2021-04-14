57cfa5f9bcb5f5be25f463205de161864a5715c7
jerobernardi <jerobernardi@gmail.com>
2021-04-14 19:40:23 -0300

    Greedy with test implemented. Keep Testing

--- mains/main_greedy.c
+++ mains/main_greedy.c
@@ -1,2 +1,19 @@
-#include "RomaVictor.h"
+#include "../RomaVictor.h"
+#include <time.h>
 
+int main(void) {
+    clock_t start, end;
+    double total;
+
+    Grafo G = ConstruccionDelGrafo();
+    for (u32 i = 0; i < NumeroDeVertices(G); i++) 
+        FijarOrden(i,G,i);  
+
+    start = clock();
+    u32 colores = Greedy(G);
+    end = clock();
+    total = (double) (end - start) / CLOCKS_PER_SEC;
+    printf("COLORES: %u\n", colores);
+    printf("TIEMPO: %f\n", total);
+    DestruccionDelGrafo(G);
+}
--- src/coloreo.c
+++ src/coloreo.c
@@ -1,5 +1,7 @@
-#include "../RomaVictor.h";
-#include "utils.h";
+#include "../RomaVictor.h"
+#include "utils.h"
+#include <string.h>
+#include <assert.h>
 
 u32 Greedy(Grafo G) {
 /* 
@@ -10,8 +12,14 @@ u32 Greedy(Grafo G) {
     u32 colorsQty = 0;
     u32 maxColors = Delta(G) + 1;
     char *usedColors = calloc(maxColors, sizeof(char));
+    if (usedColors == NULL) return error;
+    
     u32 *visitados = calloc(NumeroDeVertices(G), sizeof(u32));
-    if (visitados == NULL || usedColors == NULL) return error;
+    if(visitados == NULL) {
+        free(usedColors);
+        return error;
+    }
+    
 
     // Pinto al primer vertice con 0 y incremento la cantidad de colores.
     FijarColor(0, 0, G);
@@ -23,15 +31,14 @@ u32 Greedy(Grafo G) {
     for (u32 i = 1; i < NumeroDeVertices(G); i++) {
 
         // reset a los colores usados por los vecinos.
-        memset(usedColors, 0, maxColors);
+        memset(usedColors, '0', maxColors);
         u32 color = error;
-        for (u32 j = 0; j < i; j++) {
+        for (u32 j = 0; j < Grado(i, G); j++) {
             
             //para cada vecino visitado seteo su color como usado.
             if (visitados[OrdenVecino(j, i, G)] == 1) {
-                
                 color = ColorVecino(j, i, G);
-                usedColors[color] = 1;
+                usedColors[color] = '1';
             }
         }
 
@@ -40,28 +47,31 @@ u32 Greedy(Grafo G) {
             colorear el vertice.
         */
         color = error;
-        for (i = 0; i < colorsQty; i++) {
+        for (u32 k = 0; k < maxColors; k++) {
 
-            if (usedColors[i] == 0) {
-
-                color = i;
+            if (usedColors[k] == '0') {
+                color = k;
                 break;
             }
         }
+        assert(color != error);
 
         /*  Checkeo si tengo que utilizar un nuevo color. 
             De ser asi aumento colorsQty 
         */
-        if(color == error) {
-
-            color = colorsQty;
+        if(color == colorsQty) {
             colorsQty++;
         }
         /*  Una vez encontrado el minimo color para el vertice i, lo pinto y lo
             guardo en visitados.
         */
+    
         FijarColor(color, i, G);
+        visitados[i] = 1;
     }
+
+    free(visitados);
+    free(usedColors);
 
     return colorsQty;
 }
